03bc0b4b2f9df11c85edc9576bf6b34d0dec8433
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-01-17 08:45:40 -0500

    initial commit

---
+++ src/document_embedder.py
@@ -0,0 +1,79 @@
+
+from biochatter.vectorstore import DocumentEmbedder, DocumentReader
+from typing import Any, Dict, List, Optional
+import logging
+
+from src.utils import get_azure_embedding_deployment
+
+logger = logging.getLogger(__name__)
+
+def new_embedder_document(authKey: str, tmpFile: str, filename: str, rag_config: Any, useRAG: Optional[bool] = False):
+    api_key = authKey
+    (is_azure, azure_deployment, base_url) = get_azure_embedding_deployment()
+    rag_agent = DocumentEmbedder(
+      used=True,
+      use_prompt=useRAG,
+      chunk_size=rag_config["chunkSize"],
+      chunk_overlap=rag_config["overlapSize"],
+      split_by_characters=rag_config["splitByChar"],
+      n_results=rag_config["resultNum"],
+      api_key=api_key,     
+      is_azure=is_azure,
+      azure_deployment=azure_deployment, 
+      azure_base_url=base_url,
+      connection_args=rag_config["connectionArgs"]
+    )
+    rag_agent.connect()
+    reader = DocumentReader()
+    docs = reader.load_document(tmpFile)
+    if len(docs) > 0:
+        for doc in docs:
+            doc.metadata.update({"source": filename})
+    logger.info('save_document')
+    logger.error('save_document')
+    return rag_agent.save_document(docs)
+    
+def get_all_documents(authKey: str, connection_args: Dict, doc_ids: Optional[List[str]] = None):
+    api_key = authKey
+    (is_azure, azure_deployment, base_url) = get_azure_embedding_deployment()
+    rag_agent = DocumentEmbedder(
+        api_key=api_key,
+        connection_args=connection_args,
+        documentids_workspace=doc_ids,
+        is_azure=is_azure,
+        azure_base_url=base_url,
+        azure_deployment=azure_deployment,
+    )
+    rag_agent.connect()
+    return rag_agent.get_all_documents()
+
+def remove_document(docId: str, authKey: str, connection_args, doc_ids: Optional[List[str]] = None):
+    api_key = authKey
+    (is_azure, azure_deployment, base_url) = get_azure_embedding_deployment()
+    rag_agent = DocumentEmbedder(
+        api_key=api_key,
+        connection_args=connection_args,
+        documentids_workspace=doc_ids,
+        is_azure=is_azure,
+        azure_deployment=azure_deployment,
+        azure_base_url=base_url
+    )
+    rag_agent.connect()
+    rag_agent.remove_document(doc_id=docId)
+
+def get_connection_status(connection_args: Optional[Dict]=None, authKey: Optional[str] = None) -> bool:
+    if connection_args is None:
+        return False
+    (is_azure, azure_deployment, base_url) = get_azure_embedding_deployment()
+    try:
+        rag_agent = DocumentEmbedder(
+            api_key=authKey,
+            is_azure=is_azure,
+            azure_deployment=azure_deployment,
+            azure_base_url=base_url,
+            connection_args=connection_args
+        )
+        rag_agent.connect()
+        return True
+    except Exception as e:
+        return False
---
+++ src/utils.py
@@ -0,0 +1,38 @@
+
+from typing import List, Tuple
+import os
+
+from flask import Request
+from src.constants import OPENAI_API_KEY
+
+def parse_api_key(bearToken: str) -> str:
+    if not bearToken:
+        return ""
+    bearToken = bearToken.strip()
+    bearToken = bearToken.replace("Bearer ", "")
+    return bearToken
+
+def get_rag_agent_prompts() -> List[str]:
+    return [
+        "The user has provided additional background information from scientific "
+        "articles.",
+        "Take the following statements into account and specifically comment on "
+        "consistencies and inconsistencies with all other information available to "
+        "you: {statements}",
+    ]
+
+def get_auth(request):
+    # If OPENAI_API_KEY is provided by server, we will use it
+    if OPENAI_API_KEY in os.environ and os.environ[OPENAI_API_KEY]:
+        return os.environ[OPENAI_API_KEY]
+    
+    # Otherwise, we will parse it from request
+    auth = request.headers.get("Authorization")
+    auth = auth if auth is not None and len(auth) > 0 else ""
+    return parse_api_key(auth)
+
+def get_azure_embedding_deployment() -> Tuple[bool, str]:
+    is_azure = os.environ.get("OPENAI_API_TYPE", "") == "azure"
+    deployment = os.environ.get("OPENAI_DEPLOYMENT_NAME", "")
+    base_url = os.environ.get("AZURE_OPENAI_ENDPOINT", "")
+    return (is_azure, deployment, base_url)
