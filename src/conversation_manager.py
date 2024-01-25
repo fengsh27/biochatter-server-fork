9b4e697a7be506c4221c724a8e83c0433485de76
fengsh <shaohong.feng.78@gmail.com>
2024-01-25 12:41:27 -0500

    change doc ids attribute name according to frontend

--- src/conversation_manager.py
+++ src/conversation_manager.py
@@ -90,7 +90,7 @@ class SessionData:
             azure_deployment=azure_deployment,
             azure_endpoint=endpoint,
             connection_args=ragConfig["connectionArgs"],
-            doc_ids=ragConfig["docIds"] if "docIds" in ragConfig else None
+            documentids_workspace=ragConfig["docIdsWorkspace"] if "docIdsWorkspace" in ragConfig else None
         )
         if useRAG:
             self.chatter.rag_agent.connect()
